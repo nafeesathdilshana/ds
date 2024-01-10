5b3a72be1359961d769a4d0d6c2e96c9ffe3fda2
nafeesathdilshana <149651787+nafeesathdilshana@users.noreply.github.com>
2024-01-10 10:55:53 +0530

    Add files via upload

--- towerofhanoi.cpp
+++ towerofhanoi.cpp
@@ -2,18 +2,26 @@
 using namespace std;
 void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
 {
-if (n > 0)
-{
-towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-cout << "Move disk " << n << " from rod " << from_rod <<" to rod " << to_rod << endl;
-towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
-}
+		if (n <= 0)
+		{
+			return;
+		}
+		
+	towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
+	cout << "Move disk " << n << " from rod " << from_rod <<" to rod " << to_rod << endl;
+	towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+
 }
 int main()
 {
 int n;
 cout << "Enter the number of disks: ";
 cin >> n;
+
+if(n<0)
+{
+	cout<<"Enter a positive number";
+}
 towerOfHanoi(n, 'A', 'C', 'B');
 return 0;
 }
